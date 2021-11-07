6dc8dec9d4f4db26e2703a7e9992016237b99054
Maximiano Leite <macio.pb.cg@gmail.com>
2021-11-06 21:18:55 -0300

    adjust Entities and file tsconfig

--- backend/src/entities/task.ts
+++ backend/src/entities/task.ts
@@ -1,12 +1,19 @@
+import {v4 as uuid} from 'uuid';
+
 class Task {
-    
-    constructor (
-        public readonly id: string,
-        public name: string,
-        public tags: string,
-        public body: string,
-        public status: boolean
-    ) {}
+    public id: string = '';
+    public name: string = '';
+    public tags: string = '';
+    public body: string = '';
+    public status: boolean = true;
+
+    constructor (props: Omit<Task, 'id'>, id?: string) {
+        Object.assign(this, props);
+
+        if (!id) {
+            this.id = uuid();
+        }
+    }
 }
 
 export { Task };
